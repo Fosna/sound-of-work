b301f1498b0ac3351a4b3bd889e53be5ebe00098
Vedran Marsic <vedran.marsic@axilis.com>
2018-10-06 08:48:56 +0200

    npm start update

--- server/index.js
+++ server/index.js
@@ -5,14 +5,8 @@ var app = express();
 var request = require("request");  // To make HTTP requests at the server side
 
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
 var helmet = require('helmet');  // To change response headers
-
-// To temporarily store JSON data from GitHub and also
-// the number of connected users
-var redis = require("redis"),
-    redis_client = redis.createClient();
 
 var path = require('path');
 
@@ -23,11 +17,11 @@ const isDev = process.env.NODE_ENV !== 'production';
 // Get the intended port number, use port 8000 if not provided
 const port = argv.port || process.env.PORT || 8000;
 server.listen(port, (err) => {
-  if(err){
+  if (err) {
     return logger.error(err.message);
   }
 });
-if(isDev)
+if (isDev)
   logger.appStarted(port, 'http://localhost');
 else
   logger.appStarted(port);
@@ -46,67 +40,52 @@ app.get('/', function (req, res) {
   res.sendFile(path.resolve('app/index.html'));
 });
 
-var allClients = [];
+app.get('/events', function (req, res) {
+    var owner = 'Fosna';
+    var repo = 'git-playground';
+    var oAuthKey = 'afb9b1d7e6740e8243a7a4dc5caf593400dde02a';
 
-// When a socket connection is created
-io.on('connection', function (socket) {
-  allClients.push(socket);
-  redis_client.incr('connected_users');
-  socket.on('disconnect', function() {
-     logger.v('Got disconnect!');
-     var i = allClients.indexOf(socket);
-     allClients.splice(i, 1);
-     redis_client.decr('connected_users');
-  });
-  socket.on('error', function(){
-    logger.error('Got errored!');
-    redis_client.decr('connected_users');
-  })
+    fetchDataFromGithub(owner, repo, oAuthKey, function(data) {
+      res.json(data);
+    },
+    function(e) {
+      logger.error(e.toString());
+      res.status(500).send(e.message);
+    });
 });
 
 // Function to get events from GitHub API
-function fetchDataFromGithub(){
+function fetchDataFromGithub(owner, repo, oAuthKey, next, err) {
   var options = {
-    url: 'https://api.github.com/events',
+    url: 'https://api.github.com/repos/Fosna/git-playground/events',
     headers: {
       'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36',
-      'Authorization': 'token ' + process.env.GITHUB_OAUTH_KEY
+      'Authorization': 'token ' + 'afb9b1d7e6740e8243a7a4dc5caf593400dde02a'
     }
   };
+
   request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
+
       var stripedData = stripData(data);  // Keep only useful keys
-      allClients.forEach(function(socket){
-        if(socket != null && socket.connected == true){
-            redis_client.get('connected_users', function(err, count) {
-                if(!err && count != null){
-                    socket.volatile.json.emit('github', {data: stripedData, connected_users: count});
-                }else{
-                  logger.error(err.message);
-                }
-            });
-        }
-      });
-
-    }else{
+      next(stripedData);
+    } else {
       logger.error("GitHub status code: " + response.statusCode);
+      err("GitHub status code: " + response.statusCode);
     }
-  })
-  setTimeout(fetchDataFromGithub, 2000);
+  });
+
 }
-setTimeout(fetchDataFromGithub, 2000);
 
-
-function stripData(data){
+function stripData(data) {
   var stripedData = [];
   var pushEventCounter = 0;
-  var IssueCommentEventCounter = 0;
-  var IssuesEventCounter = 0;
-  data.forEach(function(data){
-    if(data.type == 'PushEvent'){
-      if(pushEventCounter > 3) return;
-      if(data.payload.size != 0){
+
+  data.forEach(function (data) {
+    if (data.type == 'PushEvent') {
+      if (pushEventCounter > 3) return;
+      if (data.payload.size != 0) {
         stripedData.push({
           'id': data.id,
           'type': data.type,
@@ -117,11 +96,12 @@ function stripData(data){
           'payload_size': data.payload.size,
           'message': data.payload.commits[0].message,
           'created': data.created_at,
-          'url': data.repo.url
+          'url': data.repo.url,
+          'branch': data.payload.ref
         });
         pushEventCounter++;
       }
-    }else if(data.type == 'IssueCommentEvent'){
+    } else if (data.type == 'IssueCommentEvent') {
       stripedData.push({
         'id': data.id,
         'type': data.type,
@@ -134,7 +114,7 @@ function stripData(data){
         'created': data.created_at,
         'url': data.payload.comment.html_url
       });
-    }else if(data.type == 'PullRequestEvent'){
+    } else if (data.type == 'PullRequestEvent') {
       if (data.payload.pull_request.merged) data.payload.action = 'merged';
       stripedData.push({
         'id': data.id,
@@ -146,9 +126,11 @@ function stripData(data){
         'action': data.payload.action,  // opened, reopened, closed, merged
         'message': data.payload.pull_request.title,
         'created': data.created_at,
-        'url': data.payload.pull_request.html_url
+        'url': data.payload.pull_request.html_url,
+        'base': data.payload.pull_request.base.ref,
+        'head': data.payload.pull_request.head.ref,
       });
-    }else if(data.type == 'IssuesEvent'){
+    } else if (data.type == 'IssuesEvent') {
       stripedData.push({
         'id': data.id,
         'type': data.type,
